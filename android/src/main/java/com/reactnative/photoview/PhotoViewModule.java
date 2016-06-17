e3299ac8f9e1bf5f38d321483480ea14d06d3740
alwxndr <alwxndr@gmail.com>
2016-06-17 23:59:51 +0300

    first commit

---
+++ android/src/main/java/com/reactnative/photoview/PhotoViewModule.java
@@ -0,0 +1,52 @@
+package com.reactnative.photoview;
+
+import android.graphics.drawable.Animatable;
+import com.facebook.drawee.backends.pipeline.Fresco;
+import com.facebook.drawee.backends.pipeline.PipelineDraweeControllerBuilder;
+import com.facebook.drawee.controller.BaseControllerListener;
+import com.facebook.imagepipeline.image.ImageInfo;
+import com.facebook.react.uimanager.SimpleViewManager;
+import com.facebook.react.uimanager.ThemedReactContext;
+import com.facebook.react.uimanager.annotations.ReactProp;
+import me.relex.photodraweeview.PhotoDraweeView;
+
+import javax.annotation.Nullable;
+
+/**
+ * @author alwx (http://alwx.me)
+ * @version 1.0
+ */
+public class PhotoViewModule extends SimpleViewManager<PhotoDraweeView> {
+    private static final String REACT_CLASS = "PhotoView";
+
+    private PipelineDraweeControllerBuilder mController;
+
+    @Override
+    public String getName() {
+        return REACT_CLASS;
+    }
+
+    @Override
+    protected PhotoDraweeView createViewInstance(ThemedReactContext reactContext) {
+        return new PhotoDraweeView(reactContext);
+    }
+
+    @ReactProp(name = "src")
+    public void setSource(final PhotoDraweeView view, @Nullable String source) {
+        mController = Fresco.newDraweeControllerBuilder();
+        mController.setUri(source);
+        mController.setOldController(view.getController());
+        mController.setControllerListener(new BaseControllerListener<ImageInfo>() {
+            @Override
+            public void onFinalImageSet(String id, ImageInfo imageInfo, Animatable animatable) {
+                super.onFinalImageSet(id, imageInfo, animatable);
+                if (imageInfo == null) {
+                    return;
+                }
+                view.update(imageInfo.getWidth(), imageInfo.getHeight());
+            }
+        });
+        view.setController(mController.build());
+    }
+
+}
