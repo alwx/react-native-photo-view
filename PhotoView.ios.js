ff2806a4b519cb11e385fb0cf8109306bec328a9
alwxndr <alwxndr@gmail.com>
2016-06-20 13:12:41 +0300

    iOS container fixes

--- PhotoView.ios.js
+++ PhotoView.ios.js
@@ -9,14 +9,13 @@ import {
 export default class PhotoView extends Component {
     render() {
         return (
-            <View style={{flex:1}}>
-                <ScrollView
-                    contentContainerStyle={{ alignItems:'center', justifyContent:'center'}}
-                    maximumZoomScale={this.props.maximumZoomScale}
-                    minimumZoomScale={this.props.minimumZoomScale}>
-                    <Image {...this.props}/>
-                </ScrollView>
-            </View>
+            <ScrollView
+                contentContainerStyle={{ alignItems:'center', justifyContent:'center'}}
+                centerContent={true}
+                maximumZoomScale={this.props.maximumZoomScale}
+                minimumZoomScale={this.props.minimumZoomScale}>
+                <Image {...this.props}/>
+            </ScrollView>
         );
     }
 }
