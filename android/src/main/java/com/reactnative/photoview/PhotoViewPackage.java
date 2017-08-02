7c0b654a0132cd2e2a6a1509fa04bd9fde00e659
Radek Czemerys <radko93@gmail.com>
2017-08-02 20:17:52 +0200

    Remove depreciated createJSModules @ovveride marker

--- android/src/main/java/com/reactnative/photoview/PhotoViewPackage.java
+++ android/src/main/java/com/reactnative/photoview/PhotoViewPackage.java
@@ -15,7 +15,7 @@ import java.util.List;
  * @version 1.0
  */
 public class PhotoViewPackage implements ReactPackage {
-    @Override
+    // Deprecated RN 0.47
     public List<Class<? extends JavaScriptModule>> createJSModules() {
         return Collections.emptyList();
     }
